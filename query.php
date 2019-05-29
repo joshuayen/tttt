7772b8f8259aadd4b1610f5f9a5898e70587cd3c
joshuayen <joshuayen@gmail.com>
2019-05-29 14:29:43 +0800

    Update query.php

--- query.php
+++ query.php
@@ -44,15 +44,20 @@ function private_decrypt($encrypted_text)
 
 $t1=microtime(true);
 // 將資料進行加密
-$r = public_encrypt($fullText);
-var_dump($r);
-echo "<BR>\n";
+for($i=0;$i<10;$i++){
+  $r[$i]=public_encrypt($i.$fullText);
+  #$r = public_encrypt($fullText);
+  #var_dump($r);
+  #echo "<BR>\n";
+}
 
 $t2=microtime(true);
 // 將資料進行解密
-$r = private_decrypt($r);
-var_dump($r);
-echo "<BR>\n";
+for($i=0;$i<10;$i++){
+  $rr[$i] = private_decrypt($r[$i]);
+  #var_dump($r);
+  #echo "<BR>\n";
+}
 
 $t3=microtime(true);
 echo $t2-$t1 . "sec<BR>\n";
