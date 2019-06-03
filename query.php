013276cd2e9b0c4d75404b4f45dd0cac288d03ad
joshuayen <joshuayen@gmail.com>
2019-06-03 13:45:52 +0800

    Update query.php

--- query.php
+++ query.php
@@ -66,7 +66,7 @@ $t3 = microtime(true);
 echo $t2 - $t1 . "sec<BR>\n";
 echo $t3 - $t2 . "sec<BR>\n";
 
-$ch = curl_init("http://tttt-cacti.apps.example.com/q1.php");
+$ch = curl_init("http://tttt-cacti.apps.example.com/q1.php?ss=" . $ss);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 $data = curl_exec($ch);
