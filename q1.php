5c2efbc4bf6e7ef0881d679e9356eb91cbb8e348
joshuayen <joshuayen@gmail.com>
2019-06-03 14:10:49 +0800

    Update q1.php

--- q1.php
+++ q1.php
@@ -69,5 +69,5 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 $data = curl_exec($ch);
 curl_close($ch);
-echo $data;
+echo $data . "<BR>" . microtime(true);
 ?>
